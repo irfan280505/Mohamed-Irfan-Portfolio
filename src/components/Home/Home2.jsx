df137210189c5409373f8974cfc4410d8f09e1a8
irfan280505 <irfanpathan2855@gmail.com>
2025-08-28 17:54:52 +0530

    Update Home2.jsx

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -59,7 +59,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/irfan-pathan-a035a031b/"
+                  href="https://www.linkedin.com/in/mohamed-irfan-a035a031b/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
